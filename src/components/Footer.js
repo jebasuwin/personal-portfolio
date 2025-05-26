9b9c91bf4ebb68119660083f6be4d49559dad165
Jeba Suwin <148338299+jebasuwin@users.noreply.github.com>
2025-05-26 15:33:16 +0530

    Your commit message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,6 @@ import navIcon4 from "../assets/img/nav-icon4.svg";
 import telephone from "../assets/img/telephone.svg";
 import envelope from "../assets/img/envelope.svg";
 
-
-
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -14,28 +12,34 @@ export const Footer = () => {
         <Row className="align-items-center">
           <Col size={12} sm={6}>
             <div className="contact-details">
-            <div className="social-icon">
-              <a ><img src={telephone} alt="Icon" /></a> <span style={{ verticalAlign: 'middle' }}>+91 8870481461</span>
-            </div>
-            <div className="social-icon">
-              <div>
-                <a ><img src={envelope} alt="Icon" /></a> <span style={{ verticalAlign: 'middle' }}>suwinjeba@gmail.com</span>
-              </div>              
-            </div>
+              <div className="social-icon">
+                <a><img src={telephone} alt="Icon" /></a>{" "}
+                <span style={{ verticalAlign: "middle" }}>+91 8870481461</span>
+              </div>
+              <div className="social-icon">
+                <div>
+                  <a><img src={envelope} alt="Icon" /></a>{" "}
+                  <span style={{ verticalAlign: "middle" }}>jebasuwinj@gmail.com</span>
+                </div>
+              </div>
             </div>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/jeba-suwin-3a96b922b/"><img src={navIcon1} alt="Icon" /></a>
-              {/* <a href="#"><img src={navIcon2} alt="Icon" /></a> */}
-              <a href="https://www.instagram.com/_smilling__face_/"><img src={navIcon3} alt="Icon" /></a>
-              {/* <a href="#"><img src={navIcon3} alt="Icon" /></a> */}
-              <a href="https://github.com/jebasuwin"><img src={navIcon4} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/jeba-suwin-3a96b922b/">
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a href="https://www.instagram.com/_smilling__face_/">
+                <img src={navIcon3} alt="Icon" />
+              </a>
+              <a href="https://github.com/jebasuwin">
+                <img src={navIcon4} alt="Icon" />
+              </a>
             </div>
-            <p>Copyright 2024. All Rights Reserved @Jeba Suwin</p>
+            <p>&copy; {new Date().getFullYear()}. All Rights Reserved @Jeba Suwin</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
